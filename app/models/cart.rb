d95bc31c43e29a7b921ada1e8d9f21d44ce5e777
jdebettencourt <james.s.debettencourt@gmail.com>
2018-03-12 14:16:07 -0400

    abd push

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,10 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
   
-<ul>
-	  <% @cart.line_items.each do |item| %>
-	    <li><%= item.product.title %></li>
-	  <% end %>
-	</ul>
+# <ul>
+#	  <% @cart.line_items.each do |item| %>
+#	    <li><%= item.product.title %></li>
+#	  <% end %>
+#	</ul>
 
 end
